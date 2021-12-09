fe702fb52455d1330b073a88b3515df3a6bd93de
crud0626 <sisofiy6048@gmail.com>
2021-12-09 22:48:36 +0900

    Complete playlist publishing

--- src/components/playlist/playlist.jsx
+++ src/components/playlist/playlist.jsx
@@ -8,9 +8,11 @@ class PlayList extends Component {
             <ul className={styles.playlist_container}>
                 {this.props.videos.map((video) => (
                     <li key={video.id} className={styles.playlist}>
-                        <img src={video.snippet.thumbnails.medium.url} alt="videoImage" />
-                        <h2>{video.snippet.title}</h2>
-                        <span>{video.snippet.channelTitle}</span>
+                        <img className={styles.thumbnail} src={video.snippet.thumbnails.medium.url} alt="videoImage" />
+                        <div className={styles.video_info}>
+                            <h3 className={styles.title}>{video.snippet.title}</h3>
+                            <span className={styles.desc}>{video.snippet.channelTitle}</span>
+                        </div>
                     </li>
                 ))}
             </ul>
--- src/components/videosection/videosection.jsx
+++ src/components/videosection/videosection.jsx
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
+import styles from './videosection.module.css';
 
 class VideoSection extends Component {
     render() {
         return (
             <>
-                <iframe 
-                    id="ytplayer" 
-                    type="text/html" 
-                    title='videoplayer'
-                    width="720" height="405"
-                    src="https://www.youtube.com/embed/M7lc1UVf-VE"
-                    frameBorder="0" 
-                    allowFullScreen
-                ></iframe>
+                <div className={styles.videoplayer_container}>
+                    <iframe 
+                        className={styles.videoplayer}
+                        id="ytplayer" 
+                        type="text/html" 
+                        title='videoplayer'
+                        width="720" height="405"
+                        src="https://www.youtube.com/embed/M7lc1UVf-VE"
+                        frameBorder="0" 
+                        allowFullScreen
+                    ></iframe>
+                    
+                </div>
             </>
         );
     }
