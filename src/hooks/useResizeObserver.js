54ee9d1af3e44882bf79d70f887096df9615e722
crud0626 <sisofiy6048@gmail.com>
2022-07-22 15:32:15 +0900

    Refactor: Apply debounce to useResizeObserver

--- src/hooks/useResizeObserver.js
+++ src/hooks/useResizeObserver.js
@@ -1,15 +1,20 @@
 import { useState } from "react";
 
+let timer;
+
 const useResizeObserver = (conditionSize) => {
     const [isSmall, setIsSmall] = useState(false);
     const body = document.querySelector("body");
     
     const observer = new ResizeObserver((entries) => {
-        for (let entry of entries) {
-            entry.contentRect.width > conditionSize 
-            ? setIsSmall(false) 
-            : setIsSmall(true);
-        }
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            for (let entry of entries) {
+                entry.contentRect.width > conditionSize 
+                ? setIsSmall(false) 
+                : setIsSmall(true);
+            }
+        }, 200);
     });
 
     observer.observe(body);
