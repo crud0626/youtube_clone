8e9af4fb5501d65d7c9ccaf062e645e3f995160b
crud0626 <sisofiy6048@gmail.com>
2022-11-23 12:19:31 +0900

    Refactor: Refactor CommentsWrapper component
    * Rename styles class
    
    * Modify UI logic

--- src/components/CommentsWrapper/CommentsWrapperPresenter.jsx
+++ src/components/CommentsWrapper/CommentsWrapperPresenter.jsx
@@ -7,39 +7,38 @@ import { nanoid } from 'nanoid';
 const CommentsWrapperPresenter = forwardRef(({ comments, commentsCount, isLoading, setObserver }, ref) => {
     const { items, nextPageToken } = comments;
 
+    const isAbort = items.length === 0 && !nextPageToken;
+    
     return( 
-        <div className={styles.container}>
+        <div className={styles.wrapper}>
             {
-                !items &&
-                <div className={styles.no_comments}>
-                    <p>댓글이 사용 중지되었습니다.</p>
-                </div>
-            }
-            {
-                items &&
-                <>
-                    <div className={styles.header}>
-                        <h3 className={styles.count}>
-                            {`댓글 ${Number(commentsCount).toLocaleString("en")} 개`}
-                        </h3>
+                isAbort ? (
+                    <div className={styles.no_comments}>
+                        <p>댓글이 사용 중지되었습니다.</p>
                     </div>
-                    <ul className={styles.body}>
-                        {items.map((item, index) => {
-                            const renderProp = {
-                                "key": nanoid(),
-                                "commentData" : item.snippet.topLevelComment,
-                            };
-
-                            if (!isLoading && index === items.length - 1 && nextPageToken) {
-                                renderProp.ref = ref;
-                                renderProp.setObserver = setObserver;
-                            }
-
-                            return <CommentBox {...renderProp} />;
-                        })}
-                        {isLoading && nextPageToken && <Spinner />}
-                    </ul>
-                </>
+                ) : (
+                    <>
+                        <div className={styles.header}>
+                            <h3 className={styles.count}>
+                                {`댓글 ${Number(commentsCount).toLocaleString("en")} 개`}
+                            </h3>
+                        </div>
+                        <ul className={styles.body}>
+                            {items.map((item, index) => {
+                                const isLast = index === items.length - 1 && nextPageToken;
+                                return (
+                                    <CommentBox 
+                                        key={nanoid()}
+                                        ref={isLast ? ref : null}
+                                        commentData={item.snippet.topLevelComment}
+                                        setObserver={isLast ? setObserver : null}
+                                    />
+                                );
+                            })}
+                            {isLoading && nextPageToken && <Spinner />}
+                        </ul>
+                    </>
+                )
             }
         </div>
     );
