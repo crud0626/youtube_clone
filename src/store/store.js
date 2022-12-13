86df2b9ab69237a17f61466c98682f5c35687974
crud0626 <sisofiy6048@gmail.com>
2022-12-13 21:52:28 +0900

    Fix: Fix a bug related to Redux DevTools #1
    Complete Hiding Redux DevTools at production level

--- src/store/store.js
+++ src/store/store.js
@@ -1,3 +1,8 @@
+/**
+ * production 레벨에서 Redux devTools를 숨기려면 배포 시 
+ * 환경 변수의 NODE_ENV의 값을 "production" 으로 꼭 등록해주어야 함.
+ */
+
 import { configureStore } from '@reduxjs/toolkit';
 import conditionSlice from './slice/conditionSlice';
 import userSlice from './slice/userSlice';
@@ -8,5 +13,6 @@ export const store = configureStore({
         condition: conditionSlice.reducer,
         video: videoSlice.reducer,
         user: userSlice.reducer
-    }
+    },
+    devTools: process.env.NODE_ENV !== 'production',
 });
